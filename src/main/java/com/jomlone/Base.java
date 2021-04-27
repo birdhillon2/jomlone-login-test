2f9831e4e10173ac4f7c3aad2b4d84735ae445a6
karan <93karanbir@gmail.com>
2021-04-27 16:51:06 -0400

    first-commit

---
+++ src/main/java/com/jomlone/Base.java
@@ -0,0 +1,20 @@
+package com.jomlone;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://52.60.159.184/login/");
+        driver.findElement(By.id("email")).sendKeys("93karanbir@gmail.com");
+        driver.findElement(By.id("password-field")).sendKeys("baToshinden1!");
+        driver.findElement(By.id("sbBt")).click();
+
+    }
+
+}
+
